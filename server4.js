29b17e6046d09e5570bd6bf5c7cfa25385cb0bbb
Aaron Coburn <acoburn@amherst.edu>
2014-01-03 17:13:25 -0500

    cleaned up example

--- server4.js
+++ server4.js
@@ -2,30 +2,17 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.urlencoded());
-app.use(express.json());
-app.use(express.methodOverride());
-app.use(app.router);
 
-app.get('/english', function (req, res, next) {
-  res.send('Hello World.');
-});
-
-app.get('/german', function (req, res, next) {
+app.get('/de', function (req, res) {
   res.send('Hallo Welt.');
 });
 
-app.get('/', function (req, res, next) {
+app.get('/fr', function (req, res) {
+  res.send('Bonjour Monde.');
+});
+
+app.use(function (req, res) {
   res.send('Hello World.');
-});
-
-app.use(function (req, res, next) {
-  next({msg: 'mea culpa'});
-});
-
-app.use(function(err, req, res, next) {
-  console.error(err);
-  res.send('errare humanum est');
 });
 
 app.listen(3000);
