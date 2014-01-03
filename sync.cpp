9197191552fb75f3b800738bc08b96bd97cc8e17
Aaron Coburn <acoburn@amherst.edu>
2014-01-03 17:10:10 -0500

    moved var decl to top of func

--- sync.cpp
+++ sync.cpp
@@ -8,6 +8,7 @@ int main () {
     std::ifstream ifs;
     std::string line;
     std::vector<std::string> files;
+    std::vector<std::string>::iterator it;
     
     // Add file names to a std::vector
     files.push_back("data/file1.txt");
@@ -17,7 +18,7 @@ int main () {
     files.push_back("data/file5.txt");
 
     // Iterate over filenames, open each and send content to std::cout
-    for(std::vector<std::string>::iterator it = files.begin(); it != files.end(); ++it) {
+    for(it = files.begin(); it != files.end(); ++it) {
         ifs.open(*it);
         while (std::getline(ifs, line)) {
             std::cout << line << '\n';
