db1a1f54cc275be00eb33718f27a01b4856e2a6a
Aaron Coburn <acoburn@amherst.edu>
2014-01-06 17:44:07 -0500

    reorganized directories

--- async2.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-// Iterate over filenames, open each and send content to the console
-['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'].forEach(function (file) {
-  fs.readFile('data/' + file, function (err, data) {
-    if (err) throw err;
-    console.log(data.toString());
-  });
-});
