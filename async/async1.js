36df34a5c7f197278da2cfbca01ed0b517c69472
Aaron Coburn <acoburn@amherst.edu>
2014-01-16 08:00:28 -0500

    fixed output to align with slides

--- async/async1.js
+++ async/async1.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
 
 fs.readFile('data/file1.txt', function (err, data) {
-  if (err) throw err;
-  console.log(data.toString());
+  console.log('Second');
 });
 
-console.log('---> HERE <---');
+console.log('First');
