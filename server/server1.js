655e0d5a71d94ec2c5acaf7e515e1fdbe1b26889
Aaron Coburn <acoburn@amherst.edu>
2014-01-15 23:31:14 -0500

    changed var name to app

--- server/server1.js
+++ server/server1.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
-var srv = http.createServer(function (req, res) {
+var app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello, World.');
   res.end();
 });
 
-srv.listen(3000);
+app.listen(3000);
 
